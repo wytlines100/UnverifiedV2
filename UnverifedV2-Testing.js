4fb73a59bbbfcb4bb96f18db4300c572ede70fc8
wyt <168687208+wytlines100@users.noreply.github.com>
2025-11-11 14:02:27 -0800

    Update UnverifedV2-Testing.js

--- UnverifedV2-Testing.js
+++ UnverifedV2-Testing.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         UnverifiedV2
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  :D
+// @version      2.32
+// @description  Look at my license before you modify, I WILL DMCA you.
+// @license		 Proprietary License 
 // @author       wytlines, DeadFish7, andreypidd, jet, joudaALT!
 // @match        https://miniblox.io/*
+// @match        https://miniblox.org/*
+// @match        https://miniblox.com/*
+// @match        https://blockcraft.io/*
 // @grant        none
 // ==/UserScript==
 
