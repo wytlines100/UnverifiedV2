779f96919bd4102702124473fc4964bf059b67bc
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-07-31 18:55:36 +0100

    remove unused domains

--- client.js
+++ client.js
@@ -5,11 +5,8 @@
 // @description  Look at my license before you modify, I WILL DMCA you.
 // @icon         https://raw.githubusercontent.com/wytlines100/UnverifiedV2/refs/heads/main/logo.jpg
 // @license      Proprietary License
-// @author       wytlines, DeadFish7, andreypidd, jet, joudaALT TrustIsOver
-// @match        https://miniblox.io/*
-// @match        https://miniblox.org/*
-// @match        https://miniblox.com/*
-// @match        https://blockcraft.io/*
+// @author       wytlines, DeadFish7, andreypidd, jet, joudaALT TrustIsOver, TheM1ddleM1n
+// @match        https://miniblox.io/
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @connect      api.jamendo.com
