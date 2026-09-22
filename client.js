cb8fbf5afc887f38499c06063bda1241a8597f56
PortgasDPython <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-09-22 16:04:44 +0100

    bumped to 3.3.1

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Unverified V2
 // @namespace    http://tampermonkey.net/
-// @version      3.3
+// @version      3.3.1
 // @description  Look at my license before you modify, I WILL DMCA you.
 // @icon         https://raw.githubusercontent.com/wytlines100/UnverifiedV2/refs/heads/main/logo.jpg
 // @downloadURL  https://raw.githubusercontent.com/wytlines100/UnverifiedV2/refs/heads/main/client.js
